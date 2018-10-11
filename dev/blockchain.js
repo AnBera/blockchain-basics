07853ecadc26752c04b84ba9e896df87f13cccf8
Bera <anbera@deloitte.com>
2018-10-11 20:46:58 +0530

    transaction bradcast endpoints

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,5 +1,6 @@
 const sha256 =  require('sha256');
 const currentNodeUrl = process.argv[3];
+const uuid = require('uuid/v1');
 
 function Blockchain() {
     this.chain = [];
@@ -35,12 +36,16 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
     const newTransaction = {
         amount: amount,
         sender: sender,
-        recipient: recipient
+        recipient: recipient,
+        transactionId: uuid().split('-').join('')
     };
 
-    this.pendingTransactions.push(newTransaction);
+    return newTransaction;
+};
 
-    return this.getLastBlock()['index'] + 1;
+Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj) {
+    this.pendingTransactions.push(transactionObj);
+    return this.getLastBlock()['index'] + 1; //index of the block where this transaction will be added to
 };
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
